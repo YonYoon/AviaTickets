47d669dcd1a48d69a20cc3445484934ee5d67b67
YonYoon <zhzhzh1806@gmail.com>
2024-06-04 18:04:05 +0500

    Make fetcher state object and pass it in MainTabView

--- AviaTickets/AviaTicketsApp.swift
+++ AviaTickets/AviaTicketsApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct AviaTicketsApp: App {
+    @StateObject private var fetcher = OfferCollectionFetcher()
+    
     var body: some Scene {
         WindowGroup {
             MainTabView()
+                .environmentObject(fetcher)
         }
     }
 }
