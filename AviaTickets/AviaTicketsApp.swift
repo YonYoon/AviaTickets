3645fe04b3ede8642c3f70a339c1234cb2c9190d
YonYoon <zhzhzh1806@gmail.com>
2024-06-04 17:00:12 +0500

    Initial Commit

---
+++ AviaTickets/AviaTicketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AviaTicketsApp.swift
+//  AviaTickets
+//
+//  Created by Zhansen Zhalel on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AviaTicketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
